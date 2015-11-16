1cc4ed1186b85e9bd95242cb09dff6aa684f02f1
miya0001 <miya@wpist.me>
2015-11-16 10:58:10 +0900

    some fix

--- tests/test-member-gravatars.php
+++ tests/test-member-gravatars.php
@@ -16,15 +16,34 @@ class SampleTest extends WP_UnitTestCase
 		) );
 
 		// should get all users
-		$this->assertSame(
-			"<div class=\"row member-gravatars\"><div class=\"col-xs-6 col-sm-3 gravatars\" style=\"text-align: center; margin-bottom: 20px;\"><img alt='' src='http://0.gravatar.com/avatar/96614ec98aa0c0d2ee75796dced6df54?s=96&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/96614ec98aa0c0d2ee75796dced6df54?s=192&amp;d=mm&amp;r=g 2x' class='avatar avatar-96 photo' height='96' width='96' /><br>admin</div><div class=\"col-xs-6 col-sm-3 gravatars\" style=\"text-align: center; margin-bottom: 20px;\"><img alt='' src='http://0.gravatar.com/avatar/0e457e0f02cff650f4d054d386df44bd?s=96&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/0e457e0f02cff650f4d054d386df44bd?s=192&amp;d=mm&amp;r=g 2x' class='avatar avatar-96 photo' height='96' width='96' /><br>User 2</div><div class=\"col-xs-6 col-sm-3 gravatars\" style=\"text-align: center; margin-bottom: 20px;\"><img alt='' src='http://0.gravatar.com/avatar/0adcdca8ae9043dc6597f7f4dab80522?s=96&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/0adcdca8ae9043dc6597f7f4dab80522?s=192&amp;d=mm&amp;r=g 2x' class='avatar avatar-96 photo' height='96' width='96' /><br>User 1</div></div>",
+		$this->assertContains(
+			'admin',
 			do_shortcode( '[gravatars who=""]' )
 		);
 
-		// should get users without subscriber
-		$this->assertSame(
-			"<div class=\"row member-gravatars\"><div class=\"col-xs-6 col-sm-3 gravatars\" style=\"text-align: center; margin-bottom: 20px;\"><img alt='' src='http://0.gravatar.com/avatar/96614ec98aa0c0d2ee75796dced6df54?s=96&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/96614ec98aa0c0d2ee75796dced6df54?s=192&amp;d=mm&amp;r=g 2x' class='avatar avatar-96 photo' height='96' width='96' /><br>admin</div><div class=\"col-xs-6 col-sm-3 gravatars\" style=\"text-align: center; margin-bottom: 20px;\"><img alt='' src='http://0.gravatar.com/avatar/0adcdca8ae9043dc6597f7f4dab80522?s=96&#038;d=mm&#038;r=g' srcset='http://0.gravatar.com/avatar/0adcdca8ae9043dc6597f7f4dab80522?s=192&amp;d=mm&amp;r=g 2x' class='avatar avatar-96 photo' height='96' width='96' /><br>User 1</div></div>",
+		$this->assertContains(
+			'User 1',
+			do_shortcode( '[gravatars who=""]' )
+		);
+
+		$this->assertContains(
+			'User 2',
+			do_shortcode( '[gravatars who=""]' )
+		);
+
+		$this->assertContains(
+			'admin',
 			do_shortcode( '[gravatars who="authors"]' )
+		);
+
+		$this->assertContains(
+			'User 1',
+			do_shortcode( '[gravatars who="authors"]' )
+		);
+
+		// User 2 should not be there.
+		$this->assertFalse(
+			!! preg_match( '/User 2/', do_shortcode( '[gravatars who="authors"]' ) )
 		);
 	}
 }
